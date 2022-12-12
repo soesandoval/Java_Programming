42302907aea518760d9a181094287d8611d6bc60
soesandoval <soesandoval9@hotmail.com>
2022-12-11 23:18:10 -0500

    Added all files

--- src/Day31_MethodOveriding/Car.java
+++ src/Day31_MethodOveriding/Car.java
@@ -3,7 +3,13 @@ package Day31_MethodOveriding;
 public class Car  extends Vehicle {
 
 
+    public Car(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
 
+    public void drive(){
+        System.out.println("Driving" + getBrand() + " " + getModel());
+    }
 
 
 
