35fe3c8bdd5a04492b3bd23f96de8212de2f66b3
soesandoval <soesandoval9@hotmail.com>
2022-12-13 08:59:16 -0500

    Added my last file

---
+++ src/Day31_MethodOveriding/Bike.java
@@ -0,0 +1,16 @@
+package Day31_MethodOveriding;
+
+public class Bike extends Vehicle {
+
+    public Bike(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+
+
+    public void ride(){
+        System.out.println("Riding" + getBrand() + " " + getModel());
+    }
+
+
+
+}
